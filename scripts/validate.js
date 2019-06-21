555c499fce88aa537450ab37238904c55e6e810c
Ron Buckton <ron.buckton@outlook.com>
2019-06-21 14:35:02 -0700

    Change to ascension format

--- scripts/validate.js
+++ scripts/validate.js
@@ -5,7 +5,7 @@ const schema = require("../data/schema/schema.json");
 const kleur = require("kleur");
 const validator = ajv();
 const dataDir = process.argv.length > 2 ? path.resolve(process.argv[2]) : path.resolve(__dirname, "../data");
-let errors = 0;
+let errors = [];
 for (const name of fs.readdirSync(dataDir)) {
   if (path.extname(name) !== ".json") continue;
   const file = path.join(dataDir, name);
@@ -15,8 +15,9 @@ for (const name of fs.readdirSync(dataDir)) {
     const json = JSON.parse(data);
     if (!validator.validate(schema, json)) {
       for (const error of validator.errors) {
-        process.stdout.write(`validate ${kleur.red("ERR!")} ${kleur.yellow(file)}: ${validator.errorsText([error])}\n`);
-        errors++;
+        const text = `validate ${kleur.red("ERR!")} ${kleur.yellow(file)}: ${validator.errorsText([error])}\n`;
+        process.stdout.write(text);
+        errors.push(text);
       }
     }
     else {
@@ -24,13 +25,17 @@ for (const name of fs.readdirSync(dataDir)) {
     }
   }
   catch (error) {
-    process.stdout.write(`validate ${kleur.red("ERR!")} ${kleur.yellow(file)}: ${error.message}\n`);
-    errors++;
+    const text = `validate ${kleur.red("ERR!")} ${kleur.yellow(file)}: ${error.message}\n`;
+    process.stdout.write(text);
+    errors.push(text);
   }
 }
-if (errors > 0) {
+if (errors.length > 0) {
   process.stdout.write(`validate ${kleur.red("ERR!")} Validation failed\n`);
-  process.exit(errors);
+  for (const text of errors) {
+    process.stdout.write(text);
+  }
+  process.exit(errors.leng);
 }
 else {
   process.stdout.write(`validate ${kleur.green("OK")} Validation succeeded\n`);
